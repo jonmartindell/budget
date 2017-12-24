d82965a028bfc327eb4e3919967090226498d35b
Jonathan Martindell <jmartindell@covermymeds.com>
2017-12-24 14:32:02 -0500

    work on index

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,9 @@
 class WelcomeController < ApplicationController
   def index
+    @categories = [
+      OpenStruct.new(name: "Groceries", ytd: 100, remaining: 400 ),
+      OpenStruct.new(name: "Home Maintenance", ytd: -2500, remaining: 770 ),
+      OpenStruct.new(name: "Restaurants", ytd: 30, remaining: -20 ),
+    ]
   end
 end
