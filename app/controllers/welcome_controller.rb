2ff924d22fab15aab59dfd060296412e5a0b5d5a
Jonathan Martindell <jmartindell@covermymeds.com>
2017-12-25 14:40:28 -0500

    wip views

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,9 +1,10 @@
 class WelcomeController < ApplicationController
   def index
     @categories = [
-      OpenStruct.new(name: "Groceries", ytd: 100, remaining: 400 ),
-      OpenStruct.new(name: "Home Maintenance", ytd: -2500, remaining: 770 ),
-      OpenStruct.new(name: "Restaurants", ytd: 30, remaining: -20 ),
+      OpenStruct.new(name: "Groceries", ytd: 100, remaining: 400, budget: 1000 ),
+      OpenStruct.new(name: "Home Maintenance", ytd: -2500, remaining: 770, budget: 890 ),
+      OpenStruct.new(name: "Restaurants", ytd: 30, remaining: -20, budget: 130 ),
+      OpenStruct.new(name: "Utilities", ytd: 30, remaining: 20, budget: 290 ),
     ]
   end
 end
