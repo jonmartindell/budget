80dada0d0ea5e98640a70a3d49d85183953d4fc5
Jonathan Martindell <jmartindell@covermymeds.com>
2017-12-26 11:53:46 -0500

    work?

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -6,5 +6,12 @@ class WelcomeController < ApplicationController
       OpenStruct.new(name: "Restaurants", ytd: 30, remaining: -20, budget: 130 ),
       OpenStruct.new(name: "Utilities", ytd: 30, remaining: 20, budget: 290 ),
     ]
+
+    @uncommon_categories = [
+      OpenStruct.new(name: "Groceries", ytd: 100, remaining: 400, budget: 1000 ),
+      OpenStruct.new(name: "Home Maintenance", ytd: -2500, remaining: 770, budget: 890 ),
+      OpenStruct.new(name: "Restaurants", ytd: 30, remaining: -20, budget: 130 ),
+      OpenStruct.new(name: "Utilities", ytd: 30, remaining: 20, budget: 290 ),
+    ]
   end
 end
