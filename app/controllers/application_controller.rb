ff7751b5db7fa290c3ac6b892d45b874988f1072
Jonathan Martindell <jmartindell@covermymeds.com>
2018-01-30 19:36:19 -0500

    revert

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -5,17 +5,10 @@ class ApplicationController < ActionController::Base
 
   before_action :authenticate
   def authenticate
-    # first see if they already have a session
-    if user = User.find(session[:current_user_id])
-      session[:current_user_id] = user.id
+    if user = authenticate_with_http_basic { |u, p| User.find_by(username: u) }
       @current_user = user
     else
-      if user = authenticate_with_http_basic { |u, p| User.find_by(username: u) }
-        session[:current_user_id] = user.id
-        @current_user = user
-      else
-        request_http_basic_authentication
-      end
+      request_http_basic_authentication
     end
   end
 end
