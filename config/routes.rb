01229b287cf61519fd1c9c0d461d72bce78770ce
Jonathan Martindell <jmartindell@covermymeds.com>
2018-10-01 09:26:57 -0400

    add link to summary page

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   resources :transactions
   resources :categories
-  get 'summary' => 'categories#summary'
+  get 'summary' => 'categories#summary', as: :summary
   root 'welcome#index'
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
