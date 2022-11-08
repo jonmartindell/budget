0731414ad0a04b8861199108c2af9c545b7b3d3e
Jonathan Martindell <jmartindell@covermymeds.com>
2022-11-08 13:39:36 -0500

    valid usernames

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,5 +5,3 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
-User.find_or_create(username: 'jon')
-User.find_or_create(username: 'sherry')
